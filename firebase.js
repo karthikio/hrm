d13a66e22d22da32ea370b6b82a8fb3ebc2bc932
Karthik Santhosh <developer.karthiksanthosh@gmail.com>
2025-08-14 13:27:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4TjzohLiuNwu0CMFjjCqEF74sUg4s9qM",
-  authDomain: "hrm-cube.firebaseapp.com",
-  projectId: "hrm-cube",
-  storageBucket: "hrm-cube.firebasestorage.app",
-  messagingSenderId: "863410648848",
-  appId: "1:863410648848:web:139a917dbd27816540d795"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
